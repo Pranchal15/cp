d681135593bc0f0eb3f75f64742ddb2c75d87038
Pranchal15 <pranchal.agarwal@gmail.com>
2022-04-11 00:58:42 +0530

    long done trying

--- 09-04-2022/xor.py
+++ 09-04-2022/xor.py
@@ -1,4 +1,27 @@
-# a,b,c=map(int,input().split())
-# print((a^b)+(b^c)+(a^c))
-a,b=map(int,input().split())
-print(a^b)
+a,b,c=map(int,input().split())
+print((a^b)+(b^c)+(a^c))
+# a,b=map(int,input().split())
+# print(a^b)
+# for k in range(0,(100),2):
+
+#     for i in range(0,100):
+#         for j in range(0,100):
+#             if (i^j)+i+j==k:
+#                 print(i,j,k)
+# else :print('no')
+# for x in range(2,2**10,2):
+#     print(x,end=' ')
+#     a = 0
+#     flag1 = 0
+#     flag2 = 1
+#     for b in range(1,2**10):
+#         for c in range(1,2**10):
+#             if (b^c)+b+c == x and b!=c!=a:
+#                 flag1 = 1
+#                 flag2 = 0
+#                 print(a,b,c)
+#                 break
+#         if flag1 == 1:
+#             break
+#     if flag2 == 1:
+#         print("NOPE")
--- 09-04-2022/xorabc.py
+++ 09-04-2022/xorabc.py
@@ -1,5 +1,20 @@
-x=int(input())
-if x%2==0 :
-    if (x/2)%2!=0:
-        print(1,0,int(x/2))
-    
+t=int(input())
+for _ in range(t):
+    x=int(input())
+    flag = 1
+    if x%2!=0:
+        print(-1)
+    else:
+        for j in range(0,31):
+            b = 2**j
+            a=0
+            c=int(x/2)-(b)
+            if ((a^b)+(b^c)+(c^a))==x and a!=b and b!=c and c!=a:
+                print(a,b,c)
+                flag =0
+                break
+        if flag ==1 :
+            print(-1)
+
+            
+        
